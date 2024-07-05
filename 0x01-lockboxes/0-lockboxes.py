224b571aca3eb48203e54b8593744dc50be65b2b
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-07-05 02:10:30 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -12,12 +12,12 @@ def canUnlockAll(boxes):
     opened = [False] * n
     opened[0] = True
     keys = [0]
-    
+
     while keys:
         current_key = keys.pop()
         for key in boxes[current_key]:
             if key < n and not opened[key]:
                 opened[key] = True
                 keys.append(key)
-    
+
     return all(opened)
