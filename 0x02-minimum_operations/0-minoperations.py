eed33ad67c49c49c674f386a2f86b37960b051c7
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-07-08 13:42:44 +0100

    Create 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module to calculate the fewest number of operations needed
+to result in exactly n H characters in a file.
+"""
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed
+    to result in exactly n H characters in the file.
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
