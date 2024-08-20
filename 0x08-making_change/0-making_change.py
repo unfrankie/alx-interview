3d942fe7c5ee140124a103be2a0bb2698521d0f6
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-08-20 13:09:23 +0100

    Create 0-making_change.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for making change problem
+"""
+
+def makeChange(coins, total):
+    """
+    Determine the fewest number of coins needed to meet a given amount total
+    """
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    coin_count = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        coin_count += total // coin
+        total %= coin
+    return coin_count if total == 0 else -1
