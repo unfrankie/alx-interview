a740a65f19f288912746174f8340bd4b58179fa1
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-09-03 14:24:32 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,47 +4,38 @@ Determine the winner of the Prime Game.
 """
 
 
+def is_prime_sieve(n):
+    """Helper function to create a sieve of prime numbers up to n."""
+    sieve = [True] * (n + 1)
+    sieve[0] = sieve[1] = False
+    for start in range(2, int(n**0.5) + 1):
+        if sieve[start]:
+            for multiple in range(start*start, n + 1, start):
+                sieve[multiple] = False
+    return sieve
+
+def count_primes_up_to_n(sieve, n):
+    """Count the number of primes up to n using a sieve."""
+    return sum(sieve[:n + 1])
+
 def isWinner(x, nums):
-    """
-    Determine the winner of the Prime Game.
-    """
-    def sieve_of_eratosthenes(n):
-        """Generate all prime numbers up to n using Sieve of Eratosthenes."""
-        if n < 2:
-            return []
-        sieve = [True] * (n + 1)
-        sieve[0] = sieve[1] = False
-        for i in range(2, int(n**0.5) + 1):
-            if sieve[i]:
-                for j in range(i*i, n + 1, i):
-                    sieve[j] = False
-        return [i for i in range(2, n + 1) if sieve[i]]
-
-    def play_round(n):
-        """Simulate a round of the game and return the winner."""
-        primes = sieve_of_eratosthenes(n)
-        maria_moves = ben_moves = 0
-        while primes:
-            prime = primes.pop(0)
-            maria_moves += 1
-            primes = [p for p in primes if p % prime != 0]
-            if not primes:
-                break
-            prime = primes.pop(0)
-            ben_moves += 1
-            primes = [p for p in primes if p % prime != 0]
-        return 'Maria' if maria_moves > ben_moves else 'Ben'
-
-    maria_wins = ben_wins = 0
+    """Determine the winner of the prime game."""
+    if x <= 0 or not nums:
+        return None
+    max_n = max(nums)
+    sieve = is_prime_sieve(max_n)
+    maria_wins = 0
+    ben_wins = 0
     for n in nums:
-        winner = play_round(n)
-        if winner == 'Maria':
-            maria_wins += 1
-        elif winner == 'Ben':
+        prime_count = count_primes_up_to_n(sieve, n)
+
+        if prime_count % 2 == 0:
             ben_wins += 1
+        else:
+            maria_wins += 1
     if maria_wins > ben_wins:
-        return 'Maria'
+        return "Maria"
     elif ben_wins > maria_wins:
-        return 'Ben'
+        return "Ben"
     else:
         return None
